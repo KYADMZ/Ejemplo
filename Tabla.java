7d06401ada46a09ad4cd82ead2c051590c4e8fb9
KYADMZ <118216927+KYADMZ@users.noreply.github.com>
2022-11-14 13:36:13 -0600

    Add files via upload

---
+++ Tabla.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabla;
+
+/**
+ *
+ * @author mazuk
+ */
+public class Tabla {
+
+    
+    public static void main(String[] args) {
+       System.out.println("Tabla del 5: "+"\n"+"5");
+       System.out.println("10");
+       System.out.println("15");
+       System.out.println("20");
+       System.out.println("25");
+       System.out.println("30");
+       System.out.println("35");
+       System.out.println("40");
+       System.out.println("45");
+       System.out.println("50");
+       
+       for(int i=0;i<9;i++){
+           System.out.println((i+1)*5);
+       }
+       
+    }
+    
+}
